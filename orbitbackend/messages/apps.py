ca4649add5b4a246ed948822be4f2ea6e62c4700
OUAKHILATOU <ouakhilatou@OUAKHILATOUs-MacBook-Pro.local>
2026-09-19 00:48:04 +0100

    migrate to create messages table

---
+++ orbitbackend/messages/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MessagesConfig(AppConfig):
+    name = 'orbitbackend.messages'
+    label = 'orbit_messages'
---
+++ orbitbackend/messages/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Message(models.Model):
+    class SourceType(models.TextChoices):
+        WHATSAPP = 'whatsapp', 'WhatsApp'
+        MEETING_TRANSCRIPT = 'meeting_transcript', 'Meeting Transcript'
+        PDF = 'pdf', 'PDF'
+
+    class AuthorityLevel(models.TextChoices):
+        OFFICIAL = 'official', 'Official'
+        GENERAL = 'general', 'General'
+
+    message_id = models.BigAutoField(primary_key=True)
+    source_type = models.CharField(max_length=32, choices=SourceType.choices)
+    sender = models.CharField(max_length=255)
+    timestamp = models.DateTimeField()
+    raw_text = models.TextField()
+    authority_level = models.CharField(max_length=16, choices=AuthorityLevel.choices)
+
+    class Meta:
+        db_table = 'messages'
+
+    def __str__(self):
+        return f'{self.source_type} message from {self.sender} at {self.timestamp}'
